04a6c7fe78232169039c3b927f7052874e8548f3
Andrew <150067701+AnAkfiaSaltes@users.noreply.github.com>
2024-02-11 15:50:36 +0300

    initial commit

--- project_game/main.py
+++ project_game/main.py
@@ -30,9 +30,9 @@ class Game:
                     self.running = False
 
             self.screen.fill((255, 255, 255))
-            for obstacle in self.obstacles:
-                pygame.draw.rect(self.screen, (0, 0, 0),
-                                 pygame.Rect(obstacle[0], obstacle[1], obstacle[2], obstacle[3]))
+            # for obstacle in self.obstacles:
+            #     pygame.draw.rect(self.screen, (0, 0, 0),
+            #                      pygame.Rect(obstacle[0], obstacle[1], obstacle[2], obstacle[3]))
 
             pygame.display.flip()
             self.clock.tick(60)
@@ -41,39 +41,60 @@ class Game:
         pygame.quit()
 
 
-# class Hero:
-#     def __init__(self, x, y):
-#         self.x = x
-#         self.y = y
-#         self.width = 50
-#         self.height = 50
-#         self.color = (100, 100, 100)
-#
-#     def draw(self, screen):
-#         pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
-#
-#     def move(self, dx, dy):
-#         self.x += dx
-#         self.y += dy
-#
-#     def rotate(self):
-#         # Implement rotation logic
-#         pass
-#
-#     def update(self):
-#         # Implement logic for updating hero's state
-#         pass
+class Hero:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 20
+        self.height = 20
+        self.color = (100, 100, 100)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def rotate(self):
+        pass
+
+    def update(self):
+        pass
 
 
 if __name__ == "__main__":
     game = Game()
     game.__init__()
-    game.run()
-    game.quit()
-    # hero = Hero
-
+    hero = Hero(400, 300)
     try:
-        game.run()
+        while game.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    game.running = False
+
+            hero.rotate()
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]:
+                hero.move(-5, 0)
+            if keys[pygame.K_RIGHT]:
+                hero.move(5, 0)
+            if keys[pygame.K_UP]:
+                hero.move(0, -5)
+            if keys[pygame.K_DOWN]:
+                hero.move(0, 5)
+
+            game.screen.fill((255, 255, 255))
+            for obstacle in game.obstacles:
+                pygame.draw.rect(game.screen, (0, 0, 0),
+                                 pygame.Rect(obstacle[0], obstacle[1], obstacle[2], obstacle[3]))
+
+            hero.draw(game.screen)
+
+            pygame.display.flip()
+            game.clock.tick(60)
+
     except KeyboardInterrupt:
         game.quit()
 
